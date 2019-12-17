56aa8fce9a55dfdf6297652444bf9d79d06ecdf6
NejekV2 <kamilbak113@gmail.com>
2019-12-17 21:06:22 +0100

    Generalna kopia wszystkich plików w stanie projektu w którym działa

--- gitauto.bat
+++ gitauto.bat
@@ -12,7 +12,7 @@ echo Czy wysˆa† wszystkie swoje pliki do niego     (2)?
 choice /C 12 /N /M "[1/2]:"
 
 if %ERRORLEVEL% EQU 1 goto pull
-if %ERRORLEVEL% EQU 2 goto push
+if %ERRORLEVEL% EQU 2 goto push_choice
 
 :end
 echo.
@@ -32,12 +32,21 @@ cls
 echo Nie ma potrzeby aktualizacji, wszystko aktualne
 goto end
 
+:push_choice
+echo.
+echo Chcesz wysˆa† WSZYSTKIE pliki       (1)?
+echo czy tylko te oznaczone do dodania   (2)?
+choice /C 12 /N /M "[1/2]:"
+if %ERRORLEVEL% EQU 1 goto push
+if %ERRORLEVEL% EQU 2 goto push_only
+goto end
+
 :push
 echo.
 git add *
 echo.
 set /P commit=Podaj commit tej aktualizacji: 
 git commit -m "%commit%"
-git push origin master | find /C "Already up-to-date"
-if %ERRORLEVEL% EQU 0 goto not_needed
+:push_only
+git push origin master
 goto end
