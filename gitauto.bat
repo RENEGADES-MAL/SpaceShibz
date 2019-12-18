1543666bbf6c936c0b1396d88d45776c4cd43e32
Kamil Bąk (Uczeń) <kbak281@technikumkreatywne.pl>
2019-12-18 11:43:29 +0100

    Przystosowane do startupu

--- gitauto.bat
+++ gitauto.bat
@@ -6,9 +6,10 @@ cd %workingDirectory%
 title gitauto
 cls
 echo program uruchomiony z: %workingDirectory%
+if "%1" EQU "pull" ( goto pull )
 echo.
-echo Chcesz pobraÄ‡ najnowsze dane z repozytorium   (1)?
-echo Czy wysÅ‚aÄ‡ wszystkie swoje pliki do niego     (2)?
+echo Chcesz pobra† najnowsze dane z repozytorium   (1)?
+echo Czy wysˆa† wszystkie swoje pliki do niego     (2)?
 choice /C 12 /N /M "[1/2]:"
 
 if %ERRORLEVEL% EQU 1 goto pull
@@ -16,15 +17,16 @@ if %ERRORLEVEL% EQU 2 goto push_choice
 
 :end
 echo.
-echo zakoÅ„czono prace
+echo zakoäczono prace
 echo.
 pause
 exit
 
 :pull
 echo.
-git pull origin master | find /C "Already up-to-date"
-if %ERRORLEVEL% EQU 0 goto not_needed
+echo pobieram najnowsze wersje plik¢w...
+echo.
+git pull origin master
 goto end
 
 :not_needed
@@ -34,9 +36,9 @@ goto end
 
 :push_choice
 echo.
-echo Chcesz wysÅ‚aÄ‡ WSZYSTKIE pliki ktÃ³re mogÄ… zostaÄ‡ zaktualizowane   (1)?
-echo czy tylko te oznaczone oznaczone juÅ¼ jako do dodania             (2)?
-echo czy wysÅ‚aÄ‡ juÅ¼ zapisane commit'y?                                (3)?
+echo Chcesz wysˆa† WSZYSTKIE pliki kt¢re mog¥ zosta† zaktualizowane   (1)?
+echo czy tylko te oznaczone oznaczone ju¾ jako do dodania             (2)?
+echo czy wysˆa† ju¾ zapisane commit'y?                                (3)?
 choice /C 12 /N /M "[1/2/3]:"
 if %ERRORLEVEL% EQU 1 goto push_add
 if %ERRORLEVEL% EQU 2 goto push_commit
